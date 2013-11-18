934432db8e9a6d15741c8ec7ddb6d9b24bebb302
ndfoss <nathandfoss@gmail.com>
2013-11-18 14:55:05 -0500

    Fixed action on Decrypt Button click

--- Program.java
+++ Program.java
@@ -28,7 +28,7 @@ public class Program extends JPanel{
 	public Program(){
 		final JPanel initialScreen = new JPanel(new GridLayout());
 		final Box selectBox = new Box(BoxLayout.Y_AXIS);
-		final JLabel Label = new JLabel("Encrypt Homework or Decrypt Submission");
+		final JLabel encryptLabel = new JLabel("Encrypt Homework");
 
 		final JButton encryptButton = new JButton("Encrypt");
 		encryptButton.addActionListener(new ActionListener() {
@@ -40,7 +40,7 @@ public class Program extends JPanel{
 				
 			}
 		});
-		
+		final JLabel decryptLabel = new JLabel("Decrypt Submission");
 		final JButton decryptButton = new JButton("Decrypt");
 		decryptButton.addActionListener(new ActionListener() {
 			
@@ -49,13 +49,17 @@ public class Program extends JPanel{
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				JFrame decryptFrame = new JFrame();
-				Decrypt decryptPanel = new Decrypt();
-				decryptFrame.add(decryptPanel);
-				add(decryptFrame);
+				decryptFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				decryptFrame.getContentPane().add(new Decrypt());
+				decryptFrame.setPreferredSize(new Dimension(500,500));
+		        decryptFrame.pack();
+		        decryptFrame.setVisible(true);
+				
 			}
 		});
 		
-		initialScreen.add(Label);
+		initialScreen.add(encryptLabel);
+		initialScreen.add(decryptLabel);
 		selectBox.add(encryptButton);
 		selectBox.add(decryptButton);
 		initialScreen.add(selectBox);
@@ -66,7 +70,7 @@ public class Program extends JPanel{
 		JFrame frame = new JFrame("Encryption");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().add(new Program());
-		frame.setPreferredSize(new Dimension(250,250));
+		frame.setPreferredSize(new Dimension(500,500));
         frame.pack();
         frame.setVisible(true);
 		
